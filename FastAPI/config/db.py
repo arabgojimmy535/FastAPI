bf84e6d21d52c31a7fd7ce6fc79a44f7703c2766
arangojimmy535 <39570437+arabgojimmy535@users.noreply.github.com>
2022-11-16 21:37:08 -0500

    ya esta implementado el metodo get y post

--- FastAPI/config/db.py
+++ FastAPI/config/db.py
@@ -1,2 +1,2 @@
 from pymongo import MongoClient
-conn= MongoClient()
+conn= MongoClient("mongodb+srv://arangojimmy535:3007096943@cluster0.jnjpw4y.mongodb.net/?retryWrites=true&w=majority")
--- FastAPI/routes/documentos.py
+++ FastAPI/routes/documentos.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
 from config.db import conn
 from schemas.documentos import documentoEntidad,documentosEntidad
+from models.documentos import documento
 documentos= APIRouter()
 
 @documentos.get("/documentos")
 def get_documentos():
-    return "estamos melos"
+    return documentosEntidad(conn.loslibros.documentos.find())
+
+
+@documentos.post("/documentos")
+def post_documentos(documento: documento):
+    nuevo_documento= dict(documento)
+    id= conn.loslibros.documentos.insert_one(nuevo_documento).inserted_id
+    documento= conn.loslibros.documentos.find_one({"_id": id})
+    return documentoEntidad(documento)
